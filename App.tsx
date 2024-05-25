b40df79dbbd5448e96f5663bc194cb62f85c8e10
tuan <thanhtuanse16@gmail.com>
2024-05-25 21:07:00 +0700

    refactor: update MainNavigator with new tab icons and styles

--- App.tsx
+++ App.tsx
@@ -1,23 +1,9 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { COLORS, FONTS } from './src/config'
-import { STYLES } from './src/config/styles'
-import HomeScreen from './src/screens/HomeScreen'
-import MainTabNavigator from './src/navigators/MainNavigator'
+import React from 'react';
+
+import MainTabNavigator from './src/navigators/MainNavigator';
 
 const App = () => {
-  return (
-    <MainTabNavigator/>
-  )
-}
+  return <MainTabNavigator />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: COLORS.background.primary,
-  }
-})
-
-export default App
+export default App;
