72123b4d16378748b0bb77a97741190da9a7ef72
tuan <thanhtuanse16@gmail.com>
2024-05-25 18:54:00 +0700

    fix: fix error border color displaya

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -6,6 +6,7 @@ import { BlurView } from '@react-native-community/blur';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import { FONTS } from '../config';
+import AuctionScreen from '../screens/AuctionScreen';
 
 const BottomTab = createBottomTabNavigator();
 
@@ -16,46 +17,49 @@ const MainTabNavigator = () => {
           initialRouteName="Home"
           screenOptions={{
             headerShown: false,
+            tabBarLabelStyle: {
+              color: 'white',
+              paddingVertical: 8,
+            },
             tabBarStyle: {
+              borderColor: 'transparent',
               height: 64,
               position: 'absolute',
               bottom: 16,
               left: 16,
               right: 16, 
-              borderRadius: 20,
+              elevation: 0,
               backgroundColor: 'transparent', // Make the background transparent
             },
             tabBarBackground: () => (
               <View style={{ flex: 1, backgroundColor: 'transparent',  borderRadius: 20, overflow: 'hidden' }}>
               <BlurView
-                style={{
+                  style={{
                   position: 'absolute',
                   top: 0,
                   left: 0,
                   right: 0,
                   bottom: 0,
                 }}
-                blurType="materialDark"
-                blurAmount={10}
+                blurType="dark"
+                  blurAmount={10}
+                  blurRadius={20}
               />
             </View>
             )
           }}
         >
           <BottomTab.Screen
-            options={{
-            tabBarLabel: 'Home',
-            tabBarLabelStyle: {
-              paddingVertical: 8,
-              
-              }
-            }}
             name="Home"
             component={HomeScreen}
           />
           <BottomTab.Screen
             name="Profile"
             component={ProfileScreen}
+        />
+          <BottomTab.Screen
+            name="Auction"
+            component={AuctionScreen}
           />
         </BottomTab.Navigator>
     </NavigationContainer>
